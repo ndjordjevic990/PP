5d24c2e78d0ae3f91fd0f0239191f76f2abd9626
ndjordjevic990 <n.djordjevic990@gmail.com>
2021-06-08 20:15:33 +0200

    08.06.

--- OOP_Excercises/booking-house.js
+++ OOP_Excercises/booking-house.js
@@ -11,9 +11,14 @@
   function Person(name, surname, DOB) {
     this.name = name;
     this.surname = surname;
-    this.dateOfBirth = DOB;
+    this.dateOfBirth = new Date(DOB);
     this.getData = function () {
       return this.name + " " + this.surname + " " + this.dateOfBirth;
+    };
+    this.getYearsOld = function () {
+      var year = this.dateOfBirth.getFullYear();
+      var today = new Date().getFullYear();
+      return today - year;
     };
   }
 
@@ -21,6 +26,20 @@
     this.person = person;
     this.betAmount = betAmount;
     this.country = country;
+    this.getData = function () {
+      return (
+        this.country.slice(0, 2).toUpperCase() +
+        ", " +
+        this.betAmount.toFixed(2) +
+        ", " +
+        this.person.name +
+        " " +
+        this.person.surname +
+        ", " +
+        this.person.getYearsOld() +
+        " years"
+      );
+    };
   }
 
   function Address(country, city, postalCode, street, number) {
@@ -45,6 +64,27 @@
   function BettingPlace(address) {
     this.address = address;
     this.listOfPlayers = [];
+
+    this.getData = function () {
+      var sum = 0;
+      for (var i = 0; i < this.listOfPlayers.length; i++) {
+        sum += this.listOfPlayers[i].betAmount;
+      }
+      return (
+        this.address.street +
+        ", " +
+        this.address.postalCode +
+        ", " +
+        this.address.city +
+        ", sum of all bets: " +
+        sum +
+        "eur"
+      );
+    };
+
+    this.addPlayer = function (p) {
+      this.listOfPlayers.push(p);
+    };
   }
 
   function BettingHouse(competition) {
@@ -62,9 +102,28 @@
     AUSTRALIA: "AU",
   });
 
-  var person1 = new Person("Pera", "Peric", "29.12.1991");
+  var person1 = new Person("Pera", "Peric", "1992-12-29");
+  var person2 = new Person("Nikola", "Djordjevic", "1990-12-9");
+
+  var player1 = new Player(person1, 1050, "Srbija");
+  var player2 = new Player(person2, 1050, "Srbija");
 
   var address1 = new Address("Serbia", "Beograd", 11000, "Nemanjina", 4);
-  console.log(person1.getData());
+  var address2 = new Address(
+    "Serbia",
+    "Krusevac",
+    37000,
+    "Proleterske brigade",
+    4
+  );
+
+  var serbia = new Country("Serbia", 1.3, continents.EUROPE);
+
+  var balkanBet = new BettingPlace(address1);
+  var meridian = new BettingPlace(address2);
+
+  console.log(person2);
   console.log(address1.getData());
+  console.log(player1.getData());
+  console.log(serbia);
 })();
