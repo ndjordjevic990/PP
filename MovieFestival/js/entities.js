154ff4c5c22d202384b3e11a9932bcba308c63a2
ndjordjevic990 <n.djordjevic990@gmail.com>
2021-06-19 17:21:45 +0200

    19.06.

---
+++ MovieFestival/js/entities.js
@@ -0,0 +1,55 @@
+function Festival() {
+  this.listOfAllMovies = [];
+  this.listOfPrograms = [];
+}
+//**********************MOVIE***************************************** */
+
+function Movie(title, lenght, genre) {
+  this.title = title;
+  this.lenght = parseInt(lenght);
+  this.genre = genre;
+}
+
+Movie.prototype.getGenreCode = function () {
+  return (this.name[0] + this.name[this.name.length - 1]).toUpperCase();
+};
+
+Movie.prototype.getData = function () {
+  return (
+    this.title + ", " + this.length + "min, " + this.genre.getGenreCode() + "\n"
+  );
+};
+
+//**********************PROGRAM***************************************** */
+
+function Program(date) {
+  this.date = new Date(date);
+  this.listOfMovies = [];
+}
+
+Program.prototype.lenghtOfAllMovies = function () {
+  var sumOfMinutes = 0;
+  this.listOfMovies.forEach(function (movie) {
+    sumOfMinutes += movie.lenght;
+  });
+  return sumOfMinutes;
+};
+
+Program.prototype.addMovie = function (movie) {
+  if (!movie || !(movie instanceof Movie)) {
+    console.log("Invalit input!");
+    return;
+  }
+  this.listOfMovies.push(movie);
+};
+
+Program.prototype.getTotalMovies = function () {
+  return this.listOfMovies.lenght;
+};
+
+Program.prototype.getData = function () {
+  var day = this.date.getData();
+  var month = this.date.getMonth() + 1;
+  var year = this.date.getFullYear();
+  var date = day + "." + month + "." + year;
+};
